fe92de65a2f636196d5aa381ea513f36349a3103
Meet Patel <meetpatel4302@gmail.com>
2026-07-18 09:35:26 +0530

    Update Q10.js

--- Q10.js
+++ Q10.js
@@ -1,4 +1,3 @@
-// Book Class
 class Book {
     constructor(title, author, price) {
         this.title = title;
@@ -7,7 +6,6 @@ class Book {
     }
 }
 
-// Create 5 books
 const books = [
     new Book("JavaScript", "John", 500),
     new Book("Python", "David", 600),
@@ -16,19 +14,16 @@ const books = [
     new Book("HTML", "Tim", 300)
 ];
 
-// Destructuring
 books.forEach(({ title, author, price }) => {
     console.log("Title:", title, "Author:", author, "Price:", price);
 });
 
-// Arrow function to calculate total price
 const totalPrice = (list) => {
     return list.reduce((sum, book) => sum + book.price, 0);
 };
 
 console.log("Total Price:", totalPrice(books));
 
-// Promise
 const loadBooks = () => {
     return new Promise((resolve) => {
         setTimeout(() => {
@@ -37,7 +32,6 @@ const loadBooks = () => {
     });
 };
 
-// Async/Await
 async function displayBooks() {
     console.log("Loading Books...");
     const data = await loadBooks();
