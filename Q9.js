b6f6beb44a35f3456642a5d536196dcf88500ca4
Meet Patel <meetpatel4302@gmail.com>
2026-07-18 09:34:43 +0530

    Update Q9.js

--- Q9.js
+++ Q9.js
@@ -1,4 +1,3 @@
-// Student Class
 class Student {
     constructor(name, age) {
         this.name = name;
@@ -6,7 +5,6 @@ class Student {
     }
 }
 
-// Create 5 students
 const students = [
     new Student("Riya", 20),
     new Student("Meet", 22),
@@ -15,19 +13,16 @@ const students = [
     new Student("Amit", 23)
 ];
 
-// Destructuring
 students.forEach(({ name, age }) => {
     console.log("Name:", name, "Age:", age);
 });
 
-// Arrow function to print names
 const printNames = (list) => {
     list.forEach(student => console.log(student.name));
 };
 
 printNames(students);
 
-// Promise
 const loadStudents = () => {
     return new Promise((resolve) => {
         setTimeout(() => {
@@ -36,7 +31,6 @@ const loadStudents = () => {
     });
 };
 
-// Async/Await
 async function displayStudents() {
     console.log("Loading Student Data...");
     const data = await loadStudents();
